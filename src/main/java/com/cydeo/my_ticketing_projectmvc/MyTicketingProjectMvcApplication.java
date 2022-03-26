a4d03a38f5cfe9cbc79599071edc8c082bf3ea45
RumCake01 <mia.hasnutdinova@gmail.com>
2022-03-26 08:43:31 -0400

    My ticketing project created

---
+++ src/main/java/com/cydeo/my_ticketing_projectmvc/MyTicketingProjectMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.my_ticketing_projectmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTicketingProjectMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTicketingProjectMvcApplication.class, args);
+    }
+
+}
